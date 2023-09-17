a636747d2554dbb1d028550b6a0bde21cf2a6436
aditya-dua <adityaduatechm@gmail.com>
2023-09-17 22:37:43 +0530

    Update Form.js

--- Session-8/forms-app/src/Form.js
+++ Session-8/forms-app/src/Form.js
@@ -5,10 +5,30 @@ import './App.css';
 function Form() {
   const [username, setUsername]= useState("");
   const [password, setPassword]= useState("");
+  const [errors, setErrors] = useState(null)
 
+  function getValidationErrors(){
+    let err=[];
+    if(username.trim()===""){err.push("Username cannot be empty.");}
+    if(password.trim()===""){err.push("Password cannot be empty.");}
+
+    return err;
+  }
   function submitClicked(e){
     e.preventDefault();
+    
+    const validationErrors = getValidationErrors();
+
+    if(validationErrors.length>0){
+      setErrors(validationErrors);
+      return;
+    }
     console.log("Details:"+username+"  "+password);
+    setErrors(null);
+    setUsername("");
+    setPassword("");
+    
+    
   }
 
   return (
@@ -18,6 +38,9 @@ function Form() {
         <br></br>
         <input type="text" placeholder="Enter Password" value={password} onChange={e=>setPassword(e.target.value) }></input>
 
+        <ul id="errors">
+          {errors ? errors.map((message,index)=><li key={index}>{message}</li>):null}
+        </ul>
         <input type="submit"></input>
       </form>
     </div>
